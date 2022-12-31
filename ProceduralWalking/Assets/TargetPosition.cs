56b1c0a2a5915043fed3834709ad000cbf0a8c51
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2022-12-31 09:44:40 -0500

    feat: Added 4 limbs

--- ProceduralWalking/Assets/TargetPosition.cs
+++ ProceduralWalking/Assets/TargetPosition.cs
@@ -4,22 +4,33 @@ using UnityEngine;
 
 public class TargetPosition : MonoBehaviour
 {
-    public float maxStep;
     public Transform target;
+    
     public Transform feet;
+
+    public float maxStep;
+
+    public bool moveFeet;
 
     void Update()
     {
         RaycastHit hit;
+        
         if (Physics.Raycast(transform.position, Vector3.down,  out hit, Mathf.Infinity, 1 << 7) )
         {
             target.transform.position = hit.point;
         }
-
+        
         if (Vector3.Distance (feet.position, target.position) > maxStep)
         {
+            //divide by two as the position of the feet is defined at the center of the cube
             feet.transform.position = target.transform.position + new Vector3(0, feet.transform.localScale.y / 2, 0) ;
+            moveFeet = true;
+
         }
     }
+
+
+
 
 }
