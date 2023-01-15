cbc0174f7eff1e6436c6df7a4e0976fba813be0f
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-01-15 01:04:38 -0500

    feat: added body breathing / swaying motion
    Using sine wave to allow the body to rotate up and down slightly, mimicking a life like movement

--- ProceduralWalking/Assets/Scripts/BodyMovements.cs
+++ ProceduralWalking/Assets/Scripts/BodyMovements.cs
@@ -4,23 +4,76 @@ using UnityEngine;
 
 public class BodyMovements : MonoBehaviour
 {
-    public Transform body;
 
+    public Transform body;
     public float minHeightFromGround;
+
+    public int feetCount = 8;
+    public Transform feetParent;
+    public Transform[] feets;
+    Vector3 averageFeet;
+
+    public float swayAmount;
+    float fraction;
+    float t_sway;
+    bool swayUp;
+
+    private void Start()
+    {
+        feets = new Transform[feetCount];
+
+        int i = 0;
+
+        foreach (Transform child in feetParent)
+        {
+            if (child.name.Contains("Feet"))
+            {
+                feets[i] = child.transform;
+                i++;
+            }
+        }
+    }
+
 
     void Update()
     {
-
-        RaycastHit hit;
-
-        if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, 1 << 7))
+        //adjust height of spider body based on height of feet;
+        for (int i = 0; i < feetCount; i ++)
         {
+            averageFeet += feets[i].position;
+        }
 
-            if (hit.distance != minHeightFromGround)
+        averageFeet = averageFeet / feetCount;
+
+        transform.position = new Vector3(transform.position.x, averageFeet.y + minHeightFromGround, transform.position.z);
+
+        //body sway
+        fraction += Time.deltaTime * 0.5f;
+
+        if (swayUp)
+        {
+            t_sway = Mathf.Lerp(-Mathf.PI / 2, Mathf.PI / 2, fraction);
+
+            if (fraction > 1)
             {
-                transform.position += new Vector3(0, minHeightFromGround - hit.distance, 0);
+                swayUp = false;
+                fraction = 0;
             }
         }
+        else
+        {
+            t_sway = Mathf.Lerp(Mathf.PI / 2, -Mathf.PI / 2, fraction);
+
+            if (fraction > 1)
+            {
+                swayUp = true;
+                fraction = 0;
+            }
+        }
+
+        body.rotation = new Quaternion (t_sway * swayAmount, body.rotation.y, body.rotation.z, body.rotation.w);
+
+
     }
 
 }
