a5492895303e47ff3bc043af86a763dd862bfe2d
IsaacYu15 <112666650+IsaacYu15@users.noreply.github.com>
2023-01-14 23:51:32 -0500

    Clean up code

--- ProceduralWalking/Assets/Scripts/BodyMovements.cs
+++ ProceduralWalking/Assets/Scripts/BodyMovements.cs
@@ -2,22 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AdjustBodyHeight : MonoBehaviour
+public class BodyMovements : MonoBehaviour
 {
+    public Transform body;
+    public float swaySpeed;
+    public float swayAmount;
+
     public float minHeightFromGround;
-    // Update is called once per frame
+
+    float count;
+
     void Update()
     {
+
         RaycastHit hit;
 
         if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, 1 << 7))
         {
-            //transform.rotation = new Quaternion(hit.normal.x, hit.normal.y, hit.normal.z, transform.rotation.w);
 
             if (hit.distance != minHeightFromGround)
             {
-                transform.position += new Vector3(0, (minHeightFromGround - hit.distance), 0);
+                transform.position += new Vector3(0, minHeightFromGround - hit.distance, 0);
             }
         }
     }
+
 }
